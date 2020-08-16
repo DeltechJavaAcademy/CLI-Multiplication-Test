3e13160b941de9e1d06298913a28d051f687402a
RWLangwig <rlangwig@verizon.net>
2020-08-16 09:47:20 -0400

    2 random numbers

---
+++ Multiq.java
@@ -0,0 +1,43 @@
+package multiq;
+import java.util.*;
+public class Multiq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			
+	        Scanner input = new Scanner(System.in);
+	        System.out.println("Math Time!");
+	        
+	        int firstnum = (int)(Math.random() *12);
+	        int secondnum = (int)(Math.random ()* 12);	
+	        int result1 =  firstnum * secondnum;
+		    int x = result1;
+					
+	        int comp1 =  0;
+	        int i = 0;
+	           
+	     
+	     //      While (counter < n);		
+	    //        i: 20;
+	            System.out.println("Enter result");
+	              
+	          int result2 = input.nextInt();
+	     
+	   
+	           if (result1 == result2) {
+	      	  System.out.println("Result: "  + comp1 );
+	     	
+	    //  	else if; 	        }
+	       
+	      
+	   //   System.outprintln(firstnum + " X " + secondnum " = " result1);        	
+			
+		}
+	    }
+        }
+
+	    
+
+        
