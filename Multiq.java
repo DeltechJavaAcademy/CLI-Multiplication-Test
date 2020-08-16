558b74d109d5f84794666d7eae12c079b36403f8
RWLangwig <rlangwig@verizon.net>
2020-08-16 09:52:57 -0400

    second update

--- Multiq.java
+++ Multiq.java
@@ -4,30 +4,33 @@ public class Multiq {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+				
 			
 	        Scanner input = new Scanner(System.in);
 	        System.out.println("Math Time!");
 	        
-	        int firstnum = (int)(Math.random() *12);
+	        int firstnum = (int)(Math.random() * 12);
 	        int secondnum = (int)(Math.random ()* 12);	
-	        int result1 =  firstnum * secondnum;
-		    int x = result1;
+	        
+	        System.out.println(firstnum + " " + secondnum);
+	       //int result1 =  firstnum * secondnum;
+		   // int x = result1;
+		    
+		    
 					
-	        int comp1 =  0;
-	        int i = 0;
+	  //      int comp1 =  0;
+	     //   int i = 0;
 	           
 	     
 	     //      While (counter < n);		
 	    //        i: 20;
-	            System.out.println("Enter result");
+	  //          System.out.println("Enter result");
 	              
-	          int result2 = input.nextInt();
+	//          int result2 = input.nextInt();
 	     
 	   
-	           if (result1 == result2) {
-	      	  System.out.println("Result: "  + comp1 );
+	  //         if (result1 == result2) {
+	    //  	  System.out.println("Result: "  + comp1 );
 	     	
 	    //  	else if; 	        }
 	       
@@ -36,7 +39,7 @@ public class Multiq {
 			
 		}
 	    }
-        }
+  //      }
 
 	    
 
