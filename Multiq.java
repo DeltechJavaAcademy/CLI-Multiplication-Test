8a2294d8fd7fcd2959b6c17f4608aca978cd0d60
RWLangwig <rlangwig@verizon.net>
2020-08-16 10:45:37 -0400

    display calculation

--- Multiq.java
+++ Multiq.java
@@ -10,14 +10,20 @@ public class Multiq {
 	        
 	        int firstnum = (int)(Math.random() * 12);
 	        int secondnum = (int)(Math.random ()* 12);	
+	        int calcanswer = firstnum * secondnum;
+	        		
+	          
 	        
-	        System.out.println(firstnum + "*" + secondnum + "= " );	   
+	        System.out.println(firstnum + "*" + secondnum + "= " );	  
+	        
+	        
 	        
 	        System.out.println("Enter your answer: ");
 	        
 	        int answer = input.nextInt();
 	        	             
 	        System.out.println("Your answer is " + firstnum + "*" + secondnum + "= " + answer);	  
+	        System.out.println("Correct answer: " + firstnum + "*" + secondnum + "= " + calcanswer);	
 	        
 	       //int result1 =  firstnum * secondnum;
 		   // int x = result1;
