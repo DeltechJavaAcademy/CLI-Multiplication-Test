e794d5e59b85d9504e4eae76d07abdf274962502
RWLangwig <rlangwig@verizon.net>
2020-08-16 15:43:32 -0400

    add score

--- Multiq.java
+++ Multiq.java
@@ -3,45 +3,48 @@ import java.util.*;
 public class Multiq {
 
 	public static void main(String[] args) {
-		
-			
+				
 	        Scanner input = new Scanner(System.in);
 	        System.out.println("Math Time!");
 	        
-	        int n = 21;
+	        int n = 20;
 	        int counter = 0;
+	        int p = 0;
+			int result = 0;
+	        
 	        while (counter < n) {
 	        
 	        int firstnum = (int)(Math.random() * 12);
 	        int secondnum = (int)(Math.random ()* 12);	
 	        int calcanswer = firstnum * secondnum;
 	        
-	       
-	        		
-	        
-	        
-	        System.out.println(firstnum + "*" + secondnum + "= " );	  
-	        
-	        
+	                
+	        System.out.println(firstnum + "*" + secondnum + "= " );	  	        	        
 	        
 	        System.out.println("Enter your answer: ");
 	        
 	        int answer = input.nextInt();
 	        	             
 	        System.out.println("Your answer is " + firstnum + "*" + secondnum + "= " + answer);	  
-	        System.out.println("Correct answer: " + firstnum + "*" + secondnum + "= " + calcanswer);	
-		  
-			counter++;		
-			System.out.println("Counter = " + counter);	  
-	     
-	           
-	     
-	    		
-	    //        i: 20;
+	        System.out.println("Correct answer: " + firstnum + "*" + secondnum + "= " + calcanswer);
+	        
+	        if (calcanswer == answer) {
+	        	result = result + 5; }
+	        	else { result = result + 0;}
+	        	
+			counter++;	
+			
+			System.out.println("Counter = " + counter);	 
+			
+			
+		//	System.out.println("Result: " + " " + result);
 	        
 	                 
 	   
-	  //         if (result1 == result2) {
+	     //     if (answer == calcanswer) {
+	        	  
+	    //    	  if (counter = 21) {
+	        	  System.out.println("Result: " + " " + result);
 	    //  	  System.out.println("Result: "  + comp1 );
 	     	
 	    //  	else if; 	        }
@@ -52,6 +55,7 @@ public class Multiq {
 		}
 	    }
 }
+
        
        
        
